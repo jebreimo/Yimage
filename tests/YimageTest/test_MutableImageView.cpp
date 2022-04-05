c12f4a9a0a4337b622fab67a2ff2c704287eb935
Jan Erik Breimo <jebreimo@gmail.com>
2022-04-05 21:59:42 +0200

    Add unit test for setting pixels.

--- tests/YimageTest/test_MutableImageView.cpp
+++ tests/YimageTest/test_MutableImageView.cpp
@@ -8,7 +8,7 @@
 #include <catch2/catch.hpp>
 #include "Yimage/Image.hpp"
 
-TEST_CASE("test MutableImageView::paste")
+TEST_CASE("test paste")
 {
     using namespace yimage;
     std::vector<uint8_t> buffer1{
@@ -68,5 +68,39 @@ TEST_CASE("test MutableImageView::paste")
             12, 13, 14, 15
         };
         REQUIRE(image1 == ImageView(buffer3.data(), PixelType::MONO_8, 4, 4));
+    }
+}
+
+TEST_CASE("test set_rgba8")
+{
+    using namespace yimage;
+    std::vector<uint8_t> buffer1{
+        0, 1, 2, 3,
+        4, 5, 6, 7,
+        8, 9, 10, 11,
+        12, 13, 14, 15
+    };
+    SECTION("rgba8 image")
+    {
+        MutableImageView img(buffer1.data(), PixelType::RGBA_8, 2, 2);
+        set_rgba8(img, 1, 0, {0x33, 0x77, 0xCC, 0xFF});
+        set_rgba8(img, 0, 1, {0x44, 0x88, 0xDD, 0xEE});
+        REQUIRE(buffer1[4] == 0x33);
+        REQUIRE(buffer1[7] == 0xFF);
+        REQUIRE(buffer1[9] == 0x88);
+        REQUIRE(buffer1[11] == 0xEE);
+    }
+    SECTION("rgb8 image")
+    {
+        MutableImageView img(buffer1.data(), PixelType::RGB_8, 2, 2, 2);
+        set_rgba8(img, 1, 0, {0x33, 0x77, 0xCC, 0xFF});
+        set_rgba8(img, 0, 1, {0x44, 0x88, 0xDD, 0xEE});
+        REQUIRE(buffer1[3] == 0x33);
+        REQUIRE(buffer1[5] == 0xCC);
+        REQUIRE(buffer1[6] == 6);
+        REQUIRE(buffer1[7] == 7);
+        REQUIRE(buffer1[9] == 0x88);
+        REQUIRE(buffer1[10] == 0xDD);
+        REQUIRE(buffer1[11] == 11);
     }
 }
